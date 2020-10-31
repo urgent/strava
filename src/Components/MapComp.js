912febb9def7118a2eb08d08e5f24fc548baad02
philippjohn <philipp.john98@gmail.com>
2020-10-31 13:56:50 +0000

    Trying to add onclick stuff

--- src/Components/MapComp.js
+++ src/Components/MapComp.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   withGoogleMap,
   withScriptjs,
@@ -8,24 +8,76 @@ import {
 } from "react-google-maps";
 import mapStyles from "../mapStyles";
 import * as runGeo from "../data/latLongData_activity1.json";
-import * as allData from "../data/Strava-all-1-year.json";
+import { myData } from "../data/Strava-all-1-year.js";
 
 const MapComp = (props) => {
+  const [myLat, setMyLat] = useState(48.32416);
+  const [myLng, setMyLng] = useState(10.9679);
+  const [myZoom, setMyZoom] = useState(4);
+  const [selectedActivity, setSelectedActivity] = useState(null);
+
+  const changeZoomHandler = (zoomNo) => {
+    setMyZoom(zoomNo);
+  };
+
+  useEffect(() => {
+    const listener = (e) => {
+      if (e.key === "Escape") {
+        setSelectedActivity(null);
+      }
+    };
+    window.addEventListener("keydown", listener);
+
+    return () => {
+      window.removeEventListener("keydown", listener);
+    };
+  }, []);
+
   return (
     <GoogleMap
-      defaultZoom={4}
-      defaultCenter={{ lat: 48.32416, lng: 10.9679 }}
+      zoom={myZoom}
+      center={{ lat: myLat, lng: myLng }}
       defaultOptions={{ styles: mapStyles }}
     >
-      {runGeo.coordinates.map((coords) => (
+      {myData.map((coords) => (
         <Marker
           key={coords.id}
-          position={{
-            lat: coords.latitude,
-            lng: coords.longitude,
+          onClick={() => {
+            console.log(coords.start_latitude);
+            // setMyLat(12);
+            // setMyLng(12);
+            console.log(myZoom);
+            changeZoomHandler(10);
+            console.log(myZoom);
+            //setSelectedActivity(coords);
           }}
+          position={{
+            lat: coords.start_latitude,
+            lng: coords.start_longitude,
+          }}
+          // icon={{
+          //   url: "../images/blast.svg",
+          //   scaledSize: new window.google.maps.Size(25, 25),
+          // }}
         />
       ))}
+
+      {setSelectedActivity && (
+        <InfoWindow
+          onCloseClick={() => {
+            setSelectedActivity(null);
+          }}
+          position={{
+            lat: selectedActivity.start_latitude,
+            lng: selectedActivity.start_longitude,
+          }}
+        >
+          <div>
+            <h2>{selectedActivity.name}</h2>
+            <p>{selectedActivity.moving_time}</p>
+          </div>
+        </InfoWindow>
+      )}
     </GoogleMap>
   );
 };
